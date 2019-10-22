4bd05764f1865cdcdc7bef0d0c39a377fcec09a7
AaryaRaikar <56868981+AaryaRaikar@users.noreply.github.com>
2019-10-22 14:50:38 +0530

    Add files via upload

---
+++ PRN_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() 
+{
+	//PRINTING OF PRN WITHOUT FUNCTION
+	long long prn;
+	printf("Enter your PRN:");
+	scanf("%lld",&prn);
+	printf("Your PRN is %lld",prn);
+	return 0;
+}
